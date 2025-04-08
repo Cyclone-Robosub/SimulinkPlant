da9bdfac4f294be24147642335fd883467b2130e
KoryJHaydon <138720816+KoryJHaydon@users.noreply.github.com>
2025-04-07 18:54:54 -0700

    added init file

---
+++ init.m
@@ -0,0 +1,21 @@
+% close all plots
+
+% run constants.m
+
+% run simulation
+
+% unpack data
+
+% for each simulation run create plots
+
+    % position vs time
+    % euler angle vs time
+    % velocity vs time
+    % angular velocity vs time
+    % force vector vs time
+    % thrust vector vs time
+    % PID error vs time
+    % thruster duty cycle vs time
+
+% call animation function
+
