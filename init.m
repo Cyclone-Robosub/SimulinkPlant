936aa0526b29aaab14965e415bcbe8260e5552a5
Alex Brian Zamora <149623024+abrzamora@users.noreply.github.com>
2025-04-29 21:32:14 -0700

    added prototype gain scheduler, fixed yaw cross-talk, commented out some init code for testing

--- init.m
+++ init.m
@@ -46,12 +46,12 @@ mag_noise = 0.1;
 
 
 %target states for controller
-x_des = [0, 0, 0]';
-E_des = [0, pi/6, 0]';
+x_des = [1, 1, 1]';
+E_des = [pi/12, -pi/12, pi/6]';
 states_desired = [x_des;E_des];
 
 %list of waypoints
-waypoints = [1, 0, 0];
+waypoints = [0, 0, 0];
 
 tol = 0.1; %tolerance when waypoint is considered "reached"
 
@@ -63,9 +63,9 @@ do_imu_noise_flag = 0;
 do_waypoint_control_flag = 1;
 
 %time span and step
-tspan = 30;
+tspan = 100;
 dt = 0.001; %simulation timestep
-Dt = 0.01; %controller timestep
+Dt = 0.001; %controller timestep
 plot_dt = 0.1;
 
 tic
@@ -78,7 +78,7 @@ E = squeeze(results.E.Data);
 v_e = squeeze(results.v_e.Data);
 w_b = squeeze(results.w_b.Data);
 x_e = squeeze(results.x_e.Data);
-desired_states = squeeze(results.desired_states.Data);
+desired_states = squeeze(results.desired_states.Data); 
 %current_waypoint = squeeze(results.current_waypoint.Data);
 % intermediate_waypoint = squeeze(results.intermediate_waypoint.Data);
 % for each simulation run create plots
@@ -108,47 +108,54 @@ plot(t,v_e)
 title("Velocity in NED Inertial Frame")
 xlabel("t [s]")
 ylabel("[m/s]")
+
+% %%
+% %position vs time, waypoint vs time, and intermediate waypoint vs time
+% figure
+% subplot(3,1,1)
+% plot(results.x_e.Time,results.x_e.Data(:,1))
+% hold on
+% plot(results.current_waypoint.Time,current_waypoint(1,:))
+% plot(results.intermediate_waypoint.Time,intermediate_waypoint(:,1))
+% xlabel("[s]")
+% ylabel("[m]")
+% title("X in NED")
+% subplot(3,1,2)
+% plot(results.x_e.Time,results.x_e.Data(:,2))
+% hold on
+% plot(results.current_waypoint.Time,current_waypoint(2,:))
+% plot(results.intermediate_waypoint.Time,intermediate_waypoint(:,2))
+% xlabel("[s]")
+% ylabel("[m]")
+% title("Y in NED")
+% subplot(3,1,3)
+% plot(results.x_e.Time,results.x_e.Data(:,3))
+% hold on
+% plot(results.current_waypoint.Time,current_waypoint(3,:))
+% plot(results.intermediate_waypoint.Time,intermediate_waypoint(:,3))
+% xlabel("[s]")
+% ylabel("[m]")
+% title("Z in NED")
+
 %%
-%position vs time, waypoint vs time, and intermediate waypoint vs time
-figure
-subplot(3,1,1)
-plot(results.x_e.Time,results.x_e.Data(:,1))
-hold on
-plot(results.current_waypoint.Time,current_waypoint(1,:))
-plot(results.intermediate_waypoint.Time,intermediate_waypoint(:,1))
-xlabel("[s]")
-ylabel("[m]")
-title("X in NED")
-subplot(3,1,2)
-plot(results.x_e.Time,results.x_e.Data(:,2))
-hold on
-plot(results.current_waypoint.Time,current_waypoint(2,:))
-plot(results.intermediate_waypoint.Time,intermediate_waypoint(:,2))
-xlabel("[s]")
-ylabel("[m]")
-title("Y in NED")
-subplot(3,1,3)
-plot(results.x_e.Time,results.x_e.Data(:,3))
-hold on
-plot(results.current_waypoint.Time,current_waypoint(3,:))
-plot(results.intermediate_waypoint.Time,intermediate_waypoint(:,3))
-xlabel("[s]")
-ylabel("[m]")
-title("Z in NED")
 
 %top down view
 figure
 plot3(results.x_e.Data(:,1),results.x_e.Data(:,2),results.x_e.Data(:,3))
-hold on
-plot3(current_waypoint(1,:),current_waypoint(2,:),current_waypoint(3,:),'Linestyle','none','Marker','.','MarkerSize',15)
-plot3(intermediate_waypoint(:,1),intermediate_waypoint(:,2),intermediate_waypoint(:,3),'LineStyle','none','Marker','*','MarkerSize',10)
+%hold on
+%plot3(current_waypoint(1,:),current_waypoint(2,:),current_waypoint(3,:),'Linestyle','none','Marker','.','MarkerSize',15)
+%plot3(intermediate_waypoint(:,1),intermediate_waypoint(:,2),intermediate_waypoint(:,3),'LineStyle','none','Marker','*','MarkerSize',10)
 legend("Body Position","Current WP","Intermediate WP")
-view(2)
+
+grid on
+
 xlim([-10,10])
 ylim([-10,10])
+zlim([-10,10])
 title("3D Trajectory")
 xlabel("X [m]")
 ylabel("Y [m]")
+zlabel("Z [m]")
 % angular velocity vs time
 % force vector vs time
 % thrust vector vs time
