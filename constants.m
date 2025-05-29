baa4fedd26b3df2ac20e7d0b652cc20005a46cb7
Kory Haydon <138720816+KoryJHaydon@users.noreply.github.com>
2025-05-28 20:57:57 -0700

    Updated noise and state estimators

--- constants.m
+++ constants.m
@@ -36,21 +36,23 @@ drag_wrench = [
 %currently these are hard coded in the gain scheduler
 
 %IMU Struture and constants
+
 %Accelerometer
 IMU.accelMaxRating =    10^4 * g; %[m/s^2]
 IMU.accelResolution =   122e-6 * g; % [m/s^2] convert to m/s^2/LSB
 IMU.accelOffsetBias =   0; %
 IMU.accelVelRNDWalk =   0.02 / sqrt(3600); %[(m/s^2)/sqrt(Hz)]
 IMU.accelBiasInstability = 19 * 1e-6 * g; %[m/s^2]
-IMU.accelAccelRNDWalk = 60 * 1e-6 * g ; %[(m/s^2)*sqrt(Hz)] times
+IMU.accelAccelRNDWalk = 60 * 1e-6 * g / 100;  %[(m/s^2)*sqrt(Hz)] times
+
 
 
 %Gyros
 IMU.gyroResolution =      0.0076*pi/180; %[rad/hr]
 IMU.gyroAccelBias =       0;
-IMU.gyroAngleRNDWalk =    0.16 / sqrt(3600) * 2*pi/360; %[deg/sqrt(hr)]
+IMU.gyroAngleRNDWalk =    1.5 * 0.16 / sqrt(3600) * 2*pi/360; %[deg/sqrt(hr)]
 IMU.gyroBiasInstability = 1.5 / 3600 * 2*pi / 360; %[rad/hr]
-IMU.gyroRateRNDWalk =     5 * 1e-3 * 2*pi / 360 ; %[mdps/sqrt(hr)]
+IMU.gyroRateRNDWalk =     5 * 1e-3 * 2*pi / 360 / 100; %[mdps/sqrt(hr)]
 
 %Magnetometer
 IMU.magsResolution =      0.3; %[uT]
@@ -83,17 +85,17 @@ IMU.magsAccelBias =       0; %measure this
 IMU.magsRNDWalk =         0; %measure this
 IMU.magsBiasInstability = 0; %measure this
 IMU.magsRateRNDWalk =     0; %measure this
-%}
 
+%}
 %AHRS Sensor Noise (left as default until measured)
-AHRS.accel_noise = 0.0001924722;
-AHRS.gyro_noise = 9.1385e-5;
-AHRS.mag_noise = 0.1;
+AHRS.accel_noise = 5.6037e-06;
+AHRS.gyro_noise = 2.4412e-07;
+AHRS.mag_noise = 3.5939e-05;
 AHRS.gyro_drift_noise = 3.0462e-13;
 
 %AHRS Environment Noise (left at defaults by block until I find better vals)
-AHRS.env_lin_accel_noise = 0.00962361;
-AHRS.env_mag_disturbance_noise = 0.5;
+AHRS.env_lin_accel_noise = 0.00962361/1000;
+AHRS.env_mag_disturbance_noise = 0.5/1000;
 AHRS.env_lin_accel_decay_factor = 0.5;
 AHRS.env_mag_disturbance_decay_factor = 0.5;
 AHRS.env_mag_field_strength = 50;
