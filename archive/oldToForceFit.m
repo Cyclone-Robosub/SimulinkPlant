61f5082cb0c26ef69df88d3b293b93fbf6e855f2
KoryJHaydon <138720816+KoryJHaydon@users.noreply.github.com>
2025-10-07 13:07:10 -0700

    Added full system model frame

---
+++ archive/oldToForceFit.m
@@ -0,0 +1,16 @@
+function [outputArg1,outputArg2] = oldToForceFit(inputArg1,inputArg2)
+%OLDTOFORCEFIT Summary of this function goes here
+%   Detailed explanation goes here
+arguments (Input)
+    inputArg1
+    inputArg2
+end
+
+arguments (Output)
+    outputArg1
+    outputArg2
+end
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
