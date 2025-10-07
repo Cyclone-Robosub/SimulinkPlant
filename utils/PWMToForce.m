1ed1cd5c42edf6a718033f344a1f951b781107c1
KoryJHaydon <138720816+KoryJHaydon@users.noreply.github.com>
2025-10-06 23:09:08 -0700

    Added PWMToForce (now interpolates)

---
+++ utils/PWMToForce.m
@@ -0,0 +1,24 @@
+function force = PWMToForce(pwm_input,voltage_input)
+
+    persistent interp_function %this variable must be persistant to avoid recomputing the function every time
+
+    if isempty(interp_function)
+        %search for the function is nested folders
+        file_info = dir(fullfile(pwd,'**','interp_function.mat'));
+
+        %assemble file path
+        path = fullfile(file_info.folder,file_info.name);
+
+        %load in pregenerated function
+        interp_function = load(path);
+        interp_function = interp_function.interp_function;
+    end
+    
+    %evaluate for each input pwm
+    force = zeros(size(pwm_input));
+    for i = 1:length(pwm_input)
+        force(i) = interp_function(pwm_input(i),voltage_input);
+    end
+    force = force(:);
+end
+
---
+++ utils/thrusterMaskToPWM.m
@@ -0,0 +1,7 @@
+function PWM = thrusterMaskToPWM(thruster_mask)
+
+%max thruster force is defined in constants.m
+
+%create 8x1 vector of force commands for each thruster
+thruster_forces = max_thruster_force*thruster_mask; 
+
