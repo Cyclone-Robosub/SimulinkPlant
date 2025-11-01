41ef2eeb445f55286eba03e32274f235e9bf3d19
KoryJHaydon <kjhaydon@ucdavis.edu>
2025-11-01 16:54:48 -0700

    Working on PWMToForce.

--- src/utils/T200 Thruster Lookups/PWMToForce.m
+++ src/utils/T200 Thruster Lookups/PWMToForce.m
@@ -1,24 +1,24 @@
-function force = PWMToForce(pwm_input,voltage_input)
+function force = PWMToForce(pwm_input,voltage_input,PWMgrid,voltagegrid,Forces)
 
-    persistent interp_function_PWMToForce %this variable must be persistant to avoid recomputing the function every time
+   % GridVectors 1x2 cell containing 1x201 pwms and 1x6 voltage
+   % Values 201x6 force columns for each voltage
 
-    if isempty(interp_function_PWMToForce)
-        %search for the function is nested folders
-        file_info = dir(fullfile(pwd,'**','interp_function_PWMToForce.mat'));
+   persistent func
 
-        %assemble file path
-        path = fullfile(file_info.folder,file_info.name);
+   if isempty(func)
+       
+       %compute gridded interpolant function and store in func
+       func = griddedInterpolant(PWMgrid,voltagegrid,Forces);
 
-        %load in pregenerated function
-        interp_function_PWMToForce = load(path);
-        interp_function_PWMToForce = interp_function_PWMToForce.interp_function;
-    end
+   end
+   
+   force = zeros(size(pwm_input));
+
+   for k = 1:length(pwm_input)
+       force(k) = func(pwm_input(k),voltage_input(k));
+
+   end
+
+   
     
-    %evaluate for each input pwm
-    force = zeros(size(pwm_input));
-    for i = 1:length(pwm_input)
-        force(i) = interp_function_PWMToForce(pwm_input(i),voltage_input);
-    end
-    force = force(:);
 end
-
