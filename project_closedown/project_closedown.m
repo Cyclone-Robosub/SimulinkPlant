c89215c79ad1d7b52aa9203b2e87df83bfed74a2
KoryJHaydon <138720816+KoryJHaydon@users.noreply.github.com>
2025-11-12 14:12:22 -0800

    Added project_startup and project_closedown scripts. Cleared temps.

---
+++ project_closedown/project_closedown.m
@@ -0,0 +1,11 @@
+%{
+This script is run whenever the project closes to clean up unneeded files.
+%}
+
+%clears src/temp directory
+clearTemp();
+
+%clears the automatically generated files 
+%to do
+
+fprintf("Goodbye.\n")
