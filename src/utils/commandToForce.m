7c40fc210703383dc7eb83e49e375eb6b4fe462c
KoryJHaydon <138720816+KoryJHaydon@users.noreply.github.com>
2025-11-12 16:47:58 -0800

    Worked on mode switching

--- src/utils/commandToForce.m
+++ src/utils/commandToForce.m
@@ -1,4 +1,4 @@
-function [FT_cmd_list] = commandToForce(command,max_thruster_force,invFT_wrench,invMT_wrench,ff_force_max) %#codegen
+function [FT_cmd_list] = commandToForce(command,max_thruster_force) %#codegen
 %{
 This function uses the command forward, reverse, up, down, etc... to 
 send commands corresponding to that maneuver to the right thrusters at 
@@ -7,43 +7,58 @@ the specified intensity.
 {1,0.5}
 %}
 %unpack the command
-type = command(1);
+maneuver_type = command(1);
 intensity = command(2);
+mode_id = command(3);
 
-switch(type)
-    case 2 %"+x"
-        %{
-        Tune the intensity of the pitch thrusters to cancel out the extra
-        pitching moment due to the location of the forward and backward
-        thrusters.
-        %}
-        pif = 0.2; %pitch intensity front thrusters
-        pib = -0.2; %pitch intensity back thrusters
-        thruster_mask = intensity*[pif pif pib pib 1 1 1 1]';
-        %drive forward
+switch(mode_id)
+    case 0 %Off Mode
+        FT_cmd_list = zeros(8,1);
+    case 1 %Feedforward Maneuvers Mode
+        switch(maneuver_type)
+            case 2 %"+x"
+                %{
+                Tune the intensity of the pitch thrusters to cancel out the extra
+                pitching moment due to the location of the forward and backward
+                thrusters.
+                %}
+                pif = 0.2; %pitch intensity front thrusters
+                pib = -0.2; %pitch intensity back thrusters
+                thruster_mask = intensity*[pif pif pib pib 1 1 1 1]';
+                %drive forward
+        
+            case 5 %"-x"
+                thruster_mask = intensity*[0 0 0 0 -1 -1 -1 -1]';
+                %drive backward
+            case 4 %"-yaw"
+                thruster_mask = intensity*[0 0 0 0 -1 1 -1 1]';
+                %yaw ccw
+            case 3 %"+yaw"
+                thruster_mask = intensity*[0 0 0 0 1 -.93 1 -.93]';
+                %yaw cw
+            case 6 %"-z"
+                thruster_mask = intensity*[1 1 1 1 0 0 0 0]';
+                %go up
+            case 7 %"+z"
+                thruster_mask = intensity*[-1 -1 -1 -1 0 0 0 0]';
+                %go down
+            case 0
+                thruster_mask = zeros(8,1);
+            otherwise
+                thruster_mask = zeros(8,1);
+        end
+        
+        %scale by currently allowed max_thruster_force
+        FT_cmd_list = thrusterMaskToForce(thruster_mask,max_thruster_force); 
 
-    case 5 %"-x"
-        thruster_mask = intensity*[0 0 0 0 -1 -1 -1 -1]';
-        %drive backward
-    case 4 %"-yaw"
-        thruster_mask = intensity*[0 0 0 0 -1 1 -1 1]';
-        %yaw ccw
-    case 3 %"+yaw"
-        thruster_mask = intensity*[0 0 0 0 1 -.93 1 -.93]';
-        %yaw cw
-    case 6 %"-z"
-        thruster_mask = intensity*[1 1 1 1 0 0 0 0]';
-        %go up
-    case 7 %"+z"
-        thruster_mask = intensity*[-1 -1 -1 -1 0 0 0 0]';
-        %go down
-    case 0
-        thruster_mask = zeros(8,1);
+    case 3
+        FT_cmd_list = zeros(8,1);
+        %set to zero here, to be calculated in controller
     otherwise
-        thruster_mask = zeros(8,1);
+        FT_cmd_list = zeros(8,1);
 end
 
-FT_cmd_list = thrusterMaskToForce(thruster_mask,max_thruster_force); 
+
 
 
 end
