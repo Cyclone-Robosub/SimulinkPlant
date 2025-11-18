db66ec4ba65d2c2888f0932761f2185ca22ea897
KoryJHaydon <138720816+KoryJHaydon@users.noreply.github.com>
2025-11-17 21:42:09 -0800

    vectorized vectorCross

--- src/utils/vectorCross.m
+++ src/utils/vectorCross.m
@@ -6,9 +6,7 @@ cross(V,U) = VX*U. Reference: De Ruiter - Spacecraft Dynamics and Control
 Changelog:
 Created on Nov 4, 2025 -KJH
 %}
-arguments
-    V (3,1) {mustBeNumeric}
-end
+
 VX = [0 -V(3) V(2);...
       V(3) 0 -V(1);...
       -V(2) V(1) 0];
