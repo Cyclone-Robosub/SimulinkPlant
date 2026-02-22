86e79999ea0a0929ce9cf1b91c43d706dcf5d78f
abrzamora <alexbrianzamora@gmail.com>
2026-02-21 16:43:53 -0800

    created drr and vr util functions

---
+++ src/utils/velocity_report_parser.m
@@ -0,0 +1,16 @@
+function [outputArg1,outputArg2] = velocity_report_parser(inputArg1,inputArg2)
+%VELOCITY_REPORT_PARSER Summary of this function goes here
+%   Detailed explanation goes here
+arguments (Input)
+    inputArg1
+    inputArg2
+end
+
+arguments (Output)
+    outputArg1
+    outputArg2
+end
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
