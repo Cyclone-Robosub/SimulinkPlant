d0c00698c8a188608406ab2a162d6ec3aad59fab
KoryJHaydon <138720816+KoryJHaydon@users.noreply.github.com>
2026-04-05 14:30:58 -0700

    Added better comments and annotations to init, importMission, and FB_Controller_SIM. Moved rounding of timesteps from init to src/utils/roundToSimTimestep function. Fixed error caused by empty lines at the end of the mission file.

--- src/inits/init.m
+++ src/inits/init.m
@@ -4,25 +4,28 @@ This is intended to be the one-stop-shop for setting up and running
 simulations that are run purely in Matlab and Simulink.
 
 This file can setup, run, plot, and save data from any simulation variant
-that includes the _SIM extension in the project. Depending on how the host 
-machine has been configured, this init may fail for _HIL and _CGN models.
-For these it is recommended to create a custom init for each case. Whether
-this init will work with Unreal Engine cosimulation is to be determined - 
-that may need a custom init as well. This init has not yet been tested with
-_UCS models that take advantage of Unreal Engine cosimulation.
+that includes the _SIM extension in the project. Depending on how the host
+machine has been configured, this init may fail for _HIL, _CGN, _EST, and
+_UCS models. For these it is recommended to create a custom init for each
+case.
 
 If you need to make significant modifications to this file, create a copy
 instead and give it an extension such as init_variant and place it in init
 archive.
+
+To use this codebase successfully, make sure project_startup.m has been
+added to the project settings and ran and that the project is open (i.e.
+the Project tab is visible at the top of the screen).
 %}
 
 
 %% Housekeeping and Path Management
 clc
 close all
-clear all %slows down startup so don't uncomment this unless you have a good reason to
+clear all %slow, comment this out if you don't need it
 
-if(~exist('prj_path_list','var')) %refreshes the file path in case clear all was called
+%refreshes the file path in case clear all was called
+if(~exist('prj_path_list','var')) 
     prj_path_list = getProjectPaths();
 end
 
@@ -32,21 +35,15 @@ run('constants.m') %load all necessary constants into the workspace
 
 %% Initial Conditions
 %initial intertial position
-xi_0 = 0;
-yi_0 = 0;
-zi_0 = 0;
+xi_0 = 0; yi_0 = 0; zi_0 = 0;
 Ri_0 = [xi_0; yi_0; zi_0];
 
 %initial intertial velocity
-ui_0 = 0;
-vi_0 = 0;
-wi_0 = 0;
+ui_0 = 0; vi_0 = 0; wi_0 = 0;
 dRi_0 = [ui_0; vi_0; wi_0];
 
 %initial euler angles
-phi_0 = 0;
-theta_0 = 0;
-psi_0 = 0;
+phi_0 = 0; theta_0 = 0; psi_0 = 0;
 Eul_0 = [phi_0; theta_0; psi_0]; %[roll, pitch, yaw]
 
 %other attitude representations
@@ -54,9 +51,7 @@ Cib_0 = eulToRotm(Eul_0);
 q_0 = eulToQuat(Eul_0); %[vector; scalar]
 
 %initial angular velocity
-wbx_0 = 0;
-wby_0 = 0;
-wbz_0 = 0;
+wbx_0 = 0; wby_0 = 0; wbz_0 = 0;
 wb_0 = [wbx_0; wby_0; wbz_0];
 
 %pack initial state
@@ -66,6 +61,8 @@ X0 = [Ri_0;q_0;dRi_0;wb_0];
 %TBA
 
 %% Test Conditions
+% Not all test conditions are needed for every model
+
 %battery voltage
 const_voltage = 15;
 
@@ -73,20 +70,6 @@ const_voltage = 15;
 const_joy = [0 0 0 0 0 0]'; %[Y, X ,Rise,Sink,Yaw,Pitch]
 FT_list_test = 10*[0 0 0 0 10 -10 10 -10]';
 test_pwm_list = [1500 1500 1500 1500 1500 1500 1500 1500]';
-%% Simulation Parameters
-%simulation duration
-tspan = 60;
-
-%simulation time step
-dt_sim = 1/1000;
-
-%data saving rate
-dt_data_target = 1/30;
-dt_data = round((dt_data_target/dt_sim))*dt_sim; %make sure dt_data is a multiple of dt_sim
-
-%controller update rate
-dt_control_target = 1/100;
-dt_control = round((dt_control_target/dt_sim))*dt_sim; %make sure dt_control is a multiple of dt_sim
 
 %flags are used to turn parts of the simulation on and off
 do_buoyancy_flag = 1;
@@ -96,19 +79,26 @@ do_thrusters_flag = 1;
 do_time_flag = 1; 
 do_torque_flag = 1; 
 do_force_flag = 1; 
+%% Simulation Parameters
+%simulation duration
+tspan = 60;
 
-%the mission file used for this sim
+%timesteps for various simulation components
+dt_sim = 1/1000; %sim timestep
+dt_data = roundToSimTimestep(1/30, dt_sim); %data saving timestep
+dt_control = roundToSimTimestep(1/100, dt_sim); %controller timestep
+
+%mission file and model
 mission_file_name = "mission_file_v2.txt"; 
-
-%name of the model to be simulated
-sim_select = "FB_Controller_SIM.slx";
+model_select = "FB_Controller_SIM.slx";
 
 %setup for bus objects (necessary to use structures in Simulink)
+max_commands_in_mission = 64; 
 run('setup_cmd_bus.m');
 
 %import the mission text file as an array of cmd objects
 mission_file_path = fullfile(prj_path_list.inits_path,mission_file_name);
-mission = importMission(mission_file_path);
+mission = importMission(mission_file_path, max_commands_in_mission);
 
 %% Simulation
 % data_file_prefix = string(datetime('now','Format','uu-MM-dd HH-mm-ss'));
@@ -121,7 +111,7 @@ mission = importMission(mission_file_path);
 % set_param('FB_Controller_SIM/To File','Filename',sim_mat_path);
 
 %setup the sim
-simIn = Simulink.SimulationInput(sim_select);
+simIn = Simulink.SimulationInput(model_select);
 
 %set the parameter `mission` containing all the cmd structures
 mission = Simulink.Parameter(mission);
--- src/utils/Controller/importMission.m
+++ src/utils/Controller/importMission.m
@@ -1,6 +1,6 @@
-function cmd_array = importMission(file_name)
+function cmd_array = importMission(file_name, max_commands_in_mission)
     f = fopen(file_name,"r");
-    MAX_CMDS = 64;
+    
 %define the field names for the command structure
 %these must exactly match the names and types in setup_cmd_bus.m
     f1 = 'cmd_id';
@@ -17,75 +17,114 @@ function cmd_array = importMission(file_name)
     line = fgetl(f);
 %loop until the end of the file is reached
 while ischar(line)
-        split_line = strsplit(line, {','});
-        cmd_id = strtrim(split_line{1});
-        wp = str2num(split_line{2})'; %#ok<ST2NM>
-        wp(4:6) = wrapToPi(wp(4:6)); %wrap the angular waypoints to the right domain
-        wp_mask = str2num(split_line{3})'; %#ok<ST2NM>
-        wp_tol = str2num(split_line{4})'; %#ok<ST2NM>
-        hold_time = str2num(split_line{5}); %#ok<ST2NM>
-        obj_id = strtrim(split_line{6});
-        conf = str2double(split_line{7});
-        trick_id = strtrim(split_line{8});
-        exec_timeout = str2num(split_line{9}); %#ok<ST2NM>
-        %{
-        Replace each cmd_id with the 16_char version
-        %}
-switch cmd_id
-    case 'drive_to_world_waypoint'
-        cmd_id = 'drv_to_world_wp_';
-    case 'drive_to_world_waypoint_seeking'
-        cmd_id = 'drv_to_wp_seek__';
-    case 'idle'
-        cmd_id = 'idle____________';
-    case 'track_object_waypoint'
-        cmd_id = 'track_obj_wp____';
-    case 'duration_trick'
-        cmd_id = 'duration_trick__';
-    case 'distance_trick'
-        cmd_id = 'distance_trick__';
-    otherwise
-        cmd_id = 'idle____________';
-end
-cmd_id = int8(cmd_id);
+    %break out of loop once the first empty line is reached
+    if(isempty(line)) 
+        break;
+    end
+    %split by commas
+    split_line = strsplit(line, {','});
 
-switch trick_id
-    otherwise
-            trick_id = '________________';
-end
-trick_id = int8(trick_id);
-
-switch obj_id
-    otherwise
-            obj_id = '________________';
-end
-obj_id = int8(obj_id);
+    %identifier for the command
+    cmd_id = strtrim(split_line{1});
+   
+    %waypoint [6x1], angles wrapped to pi
+    wp = str2num(split_line{2})'; %#ok<ST2NM>
+    wp(4:6) = wrapToPi(wp(4:6)); 
     
-command_k = struct(f1,cmd_id, f2, wp, ...
-    f3, wp_mask, f4, wp_tol, ...
-    f5, hold_time, f6, obj_id, f7, conf,...
-    f8, trick_id, f9, exec_timeout);
-cmd_array(k) = command_k; %#ok<AGROW>
-k = k+1;
-line = fgetl(f);
-end
-fclose(f);
+    %waypoint mask [6x1]
+    wp_mask = str2num(split_line{3})'; %#ok<ST2NM>
 
-%% Pad to MAX_CMDS with idle commands
+    %waypoint tolerance [6x1]
+    wp_tol = str2num(split_line{4})'; %#ok<ST2NM>
+
+    %hold time [1x1]
+    hold_time = str2num(split_line{5}); %#ok<ST2NM>
+
+    %object identifier
+    obj_id = strtrim(split_line{6});
+
+    %object identification figure of merit
+    conf = str2double(split_line{7});
+
+    %trick identifier for trick commands
+    trick_id = strtrim(split_line{8});
+
+    %timeout for discountExecutive
+
+    exec_timeout = str2num(split_line{9}); %#ok<ST2NM>
+    
+    
+    %replace the full cmd names with 16 char shorthands
+    switch cmd_id
+        case 'drive_to_world_waypoint'
+            cmd_id = 'drv_to_world_wp_';
+        case 'drive_to_world_waypoint_seeking'
+            cmd_id = 'drv_to_wp_seek__';
+        case 'idle'
+            cmd_id = 'idle____________';
+        case 'track_object_waypoint'
+            cmd_id = 'track_obj_wp____';
+        case 'duration_trick'
+            cmd_id = 'duration_trick__';
+        case 'distance_trick'
+            cmd_id = 'distance_trick__';
+        otherwise
+            cmd_id = 'idle____________';
+    end
+    %cast the command id to an array of int8s for the cmd class
+    cmd_id = int8(cmd_id);
+
+    %TBA - perform a similar substitution for tricks
+    switch trick_id
+        otherwise
+            trick_id = '________________';
+    end
+    trick_id = int8(trick_id);
+
+    %TBA - perform a similar substitution for objects
+    switch obj_id
+        otherwise
+            obj_id = '________________';
+    end
+    obj_id = int8(obj_id);
+    
+    %pack the command struct - this must match EXACTLY the corresponding
+    %bus that is set up in setup_cmd_bus.m!
+    command_k = struct(f1,cmd_id, f2, wp, f3, wp_mask, f4, wp_tol, f5,...
+        hold_time, f6, obj_id, f7, conf, f8, trick_id, f9, exec_timeout);
+
+    %populate the command array with this command
+    cmd_array(k) = command_k; %#ok<AGROW>
+
+    %advance to the next spot in the cmd_array and get the next line
+    k = k+1;
+    line = fgetl(f);
+end
+
+fclose(f); %close the file
+
+%{
+The size of the cmd_array must be fixed to use with Simulink. The
+discountExecutive block expects a variable called mission of size
+max_cmd_in_mission, so we will pad whatever entries have not been
+populated.
+%}
 idle_cmd = struct(...
     f1, int8('idle____________'), ...
     f2, zeros(6,1), ...
     f3, zeros(6,1), ...
     f4, zeros(6,1), ...
-    f5, 0.0, ...
+    f5, 999999999, ...
     f6, int8('________________'), ...
     f7, 0.0, ...
     f8, int8('________________'), ...
-    f9, 0.0);
+    f9, 999999999);
 
-for i = k:MAX_CMDS
+for i = k:max_commands_in_mission
     cmd_array(i) = idle_cmd;
 end
 
+%enforce the orientation
 cmd_array = cmd_array(:);
+
 end
