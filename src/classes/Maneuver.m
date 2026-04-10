ed7c0239cf07e51c3ae7b5dd5afde4a275ec2137
KoryJHaydon <138720816+KoryJHaydon@users.noreply.github.com>
2026-04-10 09:50:43 -0700

    Renamed manevuer class, moved unused functions to the archive.

--- src/classes/Maneuver.m
+++ src/classes/Maneuver.m
@@ -1,4 +1,4 @@
-classdef maneuver2
+classdef Maneuver
 %{
 The feedforward (FF) controller works by specifying the ID and intensity of 
 a maneuver in the mission file. When init is ran, each instance of the
@@ -39,7 +39,7 @@ versions of this class.
     end
 
     methods
-        function obj =  maneuver2(ID, maxManeuverForce, FT_wrench, MT_wrench, varargin)
+        function obj =  Maneuver(ID, maxManeuverForce, FT_wrench, MT_wrench, varargin)
             %Constructor
             obj.ID = ID;
             obj.maxManeuverForce = maxManeuverForce;
