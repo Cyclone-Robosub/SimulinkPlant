96a3db56b39cef8ab9b6b8547bc4f9e0a949a1c9
abrzamora <alexbrianzamora@gmail.com>
2026-04-10 20:33:09 -0700

    Changes made on integrated_joystick_controller

--- src/inits/init.m
+++ src/inits/init.m
@@ -90,7 +90,7 @@ dt_control = roundToSimTimestep(1/100, dt_sim); %controller timestep
 
 %mission file and model
 mission_file_name = "FF_prequal_mission.txt"; 
-model_select = "FB_Controller_SIM";
+model_select = "Integrated_Joystick_HIL";
 
 %setup for bus objects (necessary to use structures in Simulink)
 max_commands_in_mission = 64; 
