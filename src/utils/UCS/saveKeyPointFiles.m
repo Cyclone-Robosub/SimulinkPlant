a04537f92b2183233b0c6bec7858b65a147af53d
Liam Multhaup <liam@jmulthaup.com>
2026-04-21 05:43:36 -0700

    Added Keypoint drawing
    For confirmation that the keypoints were done correctly. Also good visual representation of what the keypoints look like.

--- src/utils/UCS/saveKeyPointFiles.m
+++ src/utils/UCS/saveKeyPointFiles.m
@@ -5,6 +5,14 @@ mkdir(fullfile(save_file_path, "CSV_Files"));
 mkdir(fullfile(save_file_path, "Pictures"));
 for i = 1:numPerspectives
     writematrix(results.keyPointsImageList(i + startDelay_KP,:), fullfile(save_file_path,"CSV_Files/frame_" + i + ".csv"));
-    imwrite(results.camL_Feed(:, :, :, i + startDelay_KP + 2), fullfile(save_file_path,"Pictures/frame_" + i + ".png"));
+    camImage = results.camL_Feed(:, :, :, i + startDelay_KP + 2);
+    imwrite(camImage, fullfile(save_file_path,"Pictures/frame_" + i + ".png"));
+    lkp = results.keyPointsImageList(i + startDelay_KP, :);
+    drawnImage = insertShape(camImage, 'Line', [lkp(2) lkp(3) lkp(5) lkp(6)], 'Color', 'y');
+    drawnImage = insertShape(drawnImage, 'Line', [lkp(2) lkp(3) lkp(8) lkp(9)], 'Color', 'y');
+    drawnImage = insertShape(drawnImage, 'Line', [lkp(5) lkp(6) lkp(11) lkp(12)], 'Color', 'y');
+    drawnImage = insertShape(drawnImage, 'Line', [lkp(11) lkp(12) lkp(8) lkp(9)], 'Color', 'y');
+    imwrite(drawnImage, fullfile(save_file_path,"Pictures/drawn_frame_" + i + ".png"));
+
 end
-clear i todayDate save_file_path;
+clear i todayDate save_file_path camImage lkp;
