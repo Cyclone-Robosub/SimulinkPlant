cdba36acc781cc00438ee3c969ba5617706ae838
Liam Multhaup <liam@jmulthaup.com>
2026-04-21 04:59:26 -0700

    Added Multiple Saves for KPCollect
    Organized the KPCollect into saves sorted by date and time.

--- src/utils/UCS/saveKeyPointFiles.m
+++ src/utils/UCS/saveKeyPointFiles.m
@@ -1,5 +1,10 @@
+todayDate = datetime();
+save_file_path = fullfile(root_path,"SavedImages/KeyPointData/Data_" + todayDate.Day + "_" + todayDate.Month + "_" + todayDate.Year + "_" + todayDate.Hour + todayDate.Minute + floor(todayDate.Second));
+mkdir(save_file_path);
+mkdir(fullfile(save_file_path, "CSV_Files"));
+mkdir(fullfile(save_file_path, "Pictures"));
 for i = 1:numPerspectives
-    writematrix(results.keyPointsImageList(i + startDelay_KP,:), fullfile(root_path,"SavedImages/KeyPointData/TextFiles/frame_" + i + ".csv"));
-    imwrite(results.camL_Feed(:, :, :, i + startDelay_KP + 2), fullfile(root_path,"SavedImages/KeyPointData/Pictures/frame_" + i + ".png"));
+    writematrix(results.keyPointsImageList(i + startDelay_KP,:), fullfile(save_file_path,"CSV_Files/frame_" + i + ".csv"));
+    imwrite(results.camL_Feed(:, :, :, i + startDelay_KP + 2), fullfile(save_file_path,"Pictures/frame_" + i + ".png"));
 end
-clear i;
+clear i todayDate save_file_path;
