4630cb439b6ab600e1931fc4624de28cc1100d4b
KoryJHaydon <kjhaydon@ucdavis.edu>
2026-05-09 12:12:58 -0700

    Added thruster allocator, currently just passes through sum.

---
+++ src/utils/Controller/thrusterAllocator.m
@@ -0,0 +1,21 @@
+function FT_cmd_list = thrusterAllocator(FT_moment_list, FT_force_list, max_thruster_force)
+%{
+Weighted thruster allocation.
+%}
+% 
+% FT_moment_list = max(-max_thruster_force, min(FT_moment_list, max_thruster_force));
+% FT_force_list = max(-max_thruster_force, min(FT_force_list, max_thruster_force));
+% 
+% [val1, ~] = max(abs(FT_moment_list));
+% [val2,~] = max(abs(FT_force_list));
+% 
+% %weighting factors for each force
+% alpha1 = val1/(val1 + val2);
+% alpha2 = val2/(val1 + val2);
+% 
+% FT_cmd_list = FT_moment_list.*alpha1 + FT_force_list.*alpha2;
+
+FT_cmd_list = FT_moment_list + FT_force_list;
+
+
+end
