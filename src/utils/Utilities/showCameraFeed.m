74f137e1ec3ceab41b2b863fc0eebe31b8260eee
KoryJHaydon <kjhaydon@ucdavis.edu>
2026-06-22 21:54:52 -0700

    Add setUnrealScenePath

--- src/utils/Utilities/showCameraFeed.m
+++ src/utils/Utilities/showCameraFeed.m
@@ -7,14 +7,18 @@ vision processing functions.
 if ~bdIsLoaded(model_select)
     open_system(model_select);
 end
-if(flag)
-    set_param([char(model_select),'/Camera Model/Simulation 3D Scene Configuration'], 'EnableWindow', 'on')
-    set_param([char(model_select),'/Camera Model/Left Camera'], 'openAtMdlStart', 'on')
-    set_param([char(model_select),'/Camera Model/Right Camera'], 'openAtMdlStart', 'on')
-else
-    set_param([char(model_select),'/Camera Model/Simulation 3D Scene Configuration'], 'EnableWindow', 'off')
-    set_param([char(model_select),'/Camera Model/Left Camera'], 'openAtMdlStart', 'off')
-    set_param([char(model_select),'/Camera Model/Right Camera'], 'openAtMdlStart', 'off')
-end
+
+try
+    if(flag)
+        set_param([char(model_select),'/Camera Model/Simulation 3D Scene Configuration'], 'EnableWindow', 'on')
+        set_param([char(model_select),'/Camera Model/Left Camera'], 'openAtMdlStart', 'on')
+        set_param([char(model_select),'/Camera Model/Right Camera'], 'openAtMdlStart', 'on')
+    else
+        set_param([char(model_select),'/Camera Model/Simulation 3D Scene Configuration'], 'EnableWindow', 'off')
+        set_param([char(model_select),'/Camera Model/Left Camera'], 'openAtMdlStart', 'off')
+        set_param([char(model_select),'/Camera Model/Right Camera'], 'openAtMdlStart', 'off')
+    end
+catch
+    fprintf("showCameraFeed did not execute. Most likely because %s does not contain camera models.\n",model_select);
 
 end
