ceaf1778c5e6b990566941e0e826154831695120
abrzamora <alexbrianzamora@gmail.com>
2026-07-05 16:14:03 -0700

    moved cgn ref subsystems into ref_subsystems

--- src/inits/init.m
+++ src/inits/init.m
@@ -163,7 +163,7 @@ dt_heartbeat = roundToSimTimestep(1/2, dt_sim);
 
 %mission file and model
 mission_file_name = "mission_file.txt"; 
-model_select = "Mission_Manager_HIL";
+model_select = "Mission_Manager_SIM";
 % open_system(model_select);
 
 % %Unreal Cosim Toggles
