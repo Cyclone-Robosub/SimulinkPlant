c90f410371cc370dbea6820dc9c7377a312106fb
KoryJHaydon <kjhaydon@ucdavis.edu>
2026-07-14 22:51:27 -0700

    added applyWPOffset stuff

--- src/mission_planning/applyWPOffset.m
+++ src/mission_planning/applyWPOffset.m
@@ -1,14 +1,18 @@
 %list of waypoints in the pool's reference frame (aligned coordinate frame)
-file_name = "example_waypoints.txt";
+measurement_list = "example_waypoints.txt";
+translated_measurement_list = "translated_waypoint.txt";
+template_xml = "template_xml.xml";
+result_xml = "result_semifinal.xml";
 
 start_position = [0.5,6.35];
-depth_target = -1;
+common_depth_target = -1;
 
+%% Read in Measurement List
 if(~exist('prj_path_list','var')) 
     prj_path_list = getProjectPaths();
 end
 
-f = fopen(file_name,"r");
+f = fopen(measurement_list,"r");
 
 fgetl(f); %skip first line
 line = fgetl(f);
@@ -28,7 +32,7 @@ while ischar(line)
     %identifier for the command
     x = str2double(strtrim(split_line{1})) - start_position(1);
     y = str2double(strtrim(split_line{2})) - start_position(2);
-    z = str2double(strtrim(split_line{3})) - depth_target;
+    z = str2double(strtrim(split_line{3})) - common_depth_target;
    
     pos = [x y z];
     pos_array = [pos_array; pos];
@@ -43,17 +47,55 @@ fclose(f); %close the file
 
 %pad with zeroes for roll, pitch, yaw
 pad_array = zeros(size(pos_array));
-
 wp_array = [pos_array, pad_array];
 
 [rows, ~] = size(wp_array);
 
+f = fopen(translated_measurement_list,'w');
 string_array = {};
-
 for k = 1:rows
     string_row = string(wp_array(k,:));
     string_array{k} = string_row(1) + "," + string_row(2) + "," + string_row(3) + ","...
         + string_row(4) + "," + string_row(5) + "," + string_row(6) + "," + string(label_array{k});
     fprintf("%s\n",string_array{k});
+    fwrite(f,sprintf("%s\n",string_array{k}));
 end
+fclose(f);
+
+%% Manually Edit Depths and Add Any Needed Rise and Descend Waypoints
+
+%% Add edited waypoints to the template xml
+
+f_in = fopen(translated_measurement_list,"r");
+line_index = 1;
+f_temp = fopen(template_xml,"r");
+f_out = fopen(result_xml,"w");
+
+fgetl(f_temp); %skip first line
+temp_line = fgetl(f_temp);
+in_line = fgetl(f_in);
+
+while(ischar(temp_line))
+   if(contains(temp_line,"DriveToWorldWaypoint"))
+        num_fields = 6;
+        in_line
+        parts = strsplit(in_line, ',');
+        new_waypoint = strjoin(parts(1:num_fields), ',');
+        
+        updated_line = regexprep(temp_line, '(world_waypoint=")[^"]*(")', ['$1' new_waypoint '$2']);
+        fwrite(f_out, updated_line);
+        fwrite(f_out,newline);     
+        in_line = fgetl(f_in);
+    else
+        fwrite(f_out,temp_line);
+        fwrite(f_out,newline);
+    end
+
+    temp_line = fgetl(f_temp);
+end
+
+
+fclose(f_in);
+fclose(f_temp);
+% fclose(f_out);
 
